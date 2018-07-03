eaea62998e42d58ac6cc1b6ffb8c6628a69dc3f1
Minh Nguyen <minhnh.91@gmail.com>
2018-07-03 16:52:14 +0200

    add vis. functions and RGB detector base class

--- mas_perception_libs/ros/src/mas_perception_libs/__init__.py
+++ mas_perception_libs/ros/src/mas_perception_libs/__init__.py
@@ -3,6 +3,8 @@ from constants import Constant
 from keras_image_classifier import KerasImageClassifier
 from image_classifier import ImageClassifier, ImageClassifierTest
 from image_recognition_service_proxy import ImageRecognitionServiceProxy
+from rgb_detector import RgbDetector, RgbDetectionKey
 from object_detector import ObjectDetector
 from detection_service_proxy import DetectionServiceProxy, DetectionServiceProxyTest, EMPTY_DETECTION_SERVICE
 import utils
+import visualization
