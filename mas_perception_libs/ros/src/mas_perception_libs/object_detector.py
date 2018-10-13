788ea14e20c3a426dc2c5916e235a9c9adff7922
Minh Nguyen <minhnh.91@gmail.com>
2018-10-13 16:39:28 +0200

    adapt object_detector module to new Plane message

--- mas_perception_libs/ros/src/mas_perception_libs/object_detector.py
+++ mas_perception_libs/ros/src/mas_perception_libs/object_detector.py
@@ -49,8 +49,16 @@ class ObjectDetector(object):
         if target_frame:
             rospy.loginfo('will transform objects and plane to target frame: ' + str(target_frame))
             for plane in self._plane_list.planes:
+                if plane.header.frame_id == target_frame:
+                    continue
+
                 # assuming only mcr_perception_msgs/Plane.pose is used
-                plane.pose = self._transform_plane(plane.pose, target_frame)
+                ps = PointStamped()
+                ps.header = plane.header
+                ps.point = plane.plane_point
+                transformed_ps = self._transform_plane(ps, target_frame)
+                plane.header = transformed_ps.header
+                plane.plane_point = transformed_ps.point
 
         if group_planes:
             planes = ObjectDetector.group_planes_by_height(self._plane_list.planes)
@@ -63,10 +71,9 @@ class ObjectDetector(object):
             plane_frame = '{0}_{1}'.format(plane_frame_prefix, plane_index)
             plane.name = plane_frame
             plane_index += 1
-            plane_quart = plane.pose.pose.orientation
 
             # make bounding boxes
-            normal = [plane_quart.x, plane_quart.y, plane_quart.z]
+            normal = [plane.coefficients[0], plane.coefficients[1], plane.coefficients[2]]
             for detected_obj in plane.object_list.objects:
                 bounding_box = BoundingBox(detected_obj.pointcloud, normal)
                 obj_pose = bounding_box.get_pose()
@@ -78,25 +85,29 @@ class ObjectDetector(object):
                 detected_obj.bounding_box = bounding_box_msg
 
             rospy.loginfo('found plane "{0}", height {1} in frame_id {2}, with {3} objects'
-                          .format(plane_frame, plane.pose.pose.position.z, plane.pose.header.frame_id,
+                          .format(plane_frame, plane.plane_point.z, plane.header.frame_id,
                                   len(plane.object_list.objects)))
 
         done_callback()
         return
 
-    def _transform_plane(self, plane_pose, target_frame):
-        """ Transform plane pose to a target frame """
+    def _transform_plane(self, plane_point, target_frame):
+        """
+        Transform plane pose to a target frame
+        :type plane_point: PointStamped
+        :type target_frame: str
+        """
         try:
-            common_time = self._tf_listener.getLatestCommonTime(target_frame, plane_pose.header.frame_id)
-            plane_pose.header.stamp = common_time
-            self._tf_listener.waitForTransform(target_frame, plane_pose.header.frame_id,
-                                               plane_pose.header.stamp, rospy.Duration(1))
+            common_time = self._tf_listener.getLatestCommonTime(target_frame, plane_point.header.frame_id)
+            plane_point.header.stamp = common_time
+            self._tf_listener.waitForTransform(target_frame, plane_point.header.frame_id,
+                                               plane_point.header.stamp, rospy.Duration(1))
 
-            plane_pose = self._tf_listener.transformPose(target_frame, plane_pose)
+            plane_point = self._tf_listener.transformPoint(target_frame, plane_point)
         except(tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            rospy.logerr('Unable to transform %s -> %s' % (plane_pose.header.frame_id, target_frame))
+            rospy.logerr('Unable to transform %s -> %s' % (plane_point.header.frame_id, target_frame))
 
-        return plane_pose
+        return plane_point
 
     def _transform_object(self, box_msg, obj_pose, target_frame):
         """ Transform object poses and box message to a target frame """
@@ -140,24 +151,24 @@ class ObjectDetector(object):
 
         for plane in planes:
             if len(plane_group_heights) == 0:
-                plane_group_heights[0] = [plane.pose.pose.position.z]
+                plane_group_heights[0] = [plane.plane_point.z]
                 plane_groups[0] = [plane_index]
             else:
                 grouped = False
                 for index in plane_group_heights:
                     avg_height = sum(plane_group_heights[index])/len(plane_group_heights[index])
-                    if abs(plane.pose.pose.position.z - avg_height) < group_threshold:
+                    if abs(plane.plane_point.z - avg_height) < group_threshold:
                         rospy.loginfo('grouping plane {0} with planes {1}'
                                       .format(plane_index, plane_groups[index]))
                         plane_groups[index].append(plane_index)
-                        plane_group_heights[index].append(plane.pose.pose.position.z)
+                        plane_group_heights[index].append(plane.plane_point.z)
                         grouped = True
                         break
                     pass
 
                 if not grouped:
                     new_index = max(plane_group_heights.keys()) + 1
-                    plane_group_heights[new_index] = [plane.pose.pose.position.z]
+                    plane_group_heights[new_index] = [plane.plane_point.z]
                     plane_groups[new_index] = [plane_index]
                     pass
                 pass
@@ -168,7 +179,7 @@ class ObjectDetector(object):
         for index in plane_groups:
             plane = planes[plane_groups[index][0]]
             avg_height = sum(plane_group_heights[index])/len(plane_group_heights[index])
-            plane.pose.pose.position.z = avg_height
+            plane.plane_point.z = avg_height
             for plane_index in plane_groups[index][1:]:
                 plane.object_list.objects.extend(planes[plane_index].object_list.objects)
 
--- mas_perception_libs/ros/src/mas_perception_libs/scene_detection_action.py
+++ mas_perception_libs/ros/src/mas_perception_libs/scene_detection_action.py
@@ -2,12 +2,14 @@ from abc import ABCMeta, abstractmethod
 import os
 import rospy
 import tf
+from dynamic_reconfigure.server import Server as ParamServer
 from actionlib import SimpleActionServer
 from cv_bridge import CvBridge
 from sensor_msgs.msg import PointCloud2
-from mcr_perception_msgs.msg import DetectSceneAction, DetectSceneResult, Plane, Object
+from mcr_perception_msgs.msg import DetectSceneAction, DetectSceneResult, PlaneList, Object
+from mas_perception_libs.cfg import PlaneFittingConfig
 from .image_detector import ImageDetectorBase, SingleImageDetectionHandler
-from .utils import cloud_msg_to_image_msg, transform_point_cloud, crop_organized_cloud_msg
+from .utils import PlaneSegmenter, cloud_msg_to_image_msg, transform_cloud_with_listener, crop_organized_cloud_msg
 
 
 class SceneDetectionActionServer(object):
@@ -31,13 +33,16 @@ class SceneDetectionActionServer(object):
 
 
 class ImageDetectionActionServer(SceneDetectionActionServer):
-    _detector_handler = None    # type: SingleImageDetectionHandler
-    _cloud_topic = None         # type: str
-    _cloud_sub = None           # type: rospy.Subscriber
-    _cloud_msg = None           # type: PointCloud2
-    _tf_listener = None         # type: tf.TransformListener
-    _target_frame = None        # type: str
-    _cv_bridge = None           # type: CvBridge
+    _detector_handler = None            # type: SingleImageDetectionHandler
+    _plane_segmenter = None             # type: PlaneSegmenter
+    _plane_fitting_param_server = None  # type: ParamServer
+    _cloud_topic = None                 # type: str
+    _cloud_sub = None                   # type: rospy.Subscriber
+    _filtered_cloud_pub = None          # type: rospy.Publisher
+    _cloud_msg = None                   # type: PointCloud2
+    _tf_listener = None                 # type: tf.TransformListener
+    _target_frame = None                # type: str
+    _cv_bridge = None                   # type: CvBridge
 
     def __init__(self, action_name, **kwargs):
         super(ImageDetectionActionServer, self).__init__(action_name, **kwargs)
@@ -55,12 +60,19 @@ class ImageDetectionActionServer(SceneDetectionActionServer):
         if not kwargs_file or not os.path.exists(kwargs_file):
             raise ValueError('invalid value for "kwargs_file": ' + kwargs_file)
 
+        # Plane segmentation
+        self._plane_segmenter = PlaneSegmenter()
+        self._plane_fitting_param_server = ParamServer(PlaneFittingConfig, self._plane_fitting_config_cb)
+
+        # image detection
         self._detector_handler = SingleImageDetectionHandler(detection_class, class_annotation_file, kwargs_file,
                                                              '/mas_perception/detection_result')
 
         self._cloud_topic = kwargs.get('cloud_topic', None)
         if not self._cloud_topic:
             raise ValueError('no cloud topic specified')
+
+        self._filtered_cloud_pub = rospy.Publisher('filtered_cloud', PointCloud2, queue_size=1)
 
         # TODO(minhnh): target_frame could be the action goal
         self._tf_listener = tf.TransformListener()
@@ -89,44 +101,41 @@ class ImageDetectionActionServer(SceneDetectionActionServer):
 
         rospy.loginfo('transforming cloud to frame: ' + self._target_frame)
         try:
-            transformed_cloud_msg = self._transform_cloud(cloud_msg)
+            transformed_cloud_msg = transform_cloud_with_listener(cloud_msg, self._target_frame, self._tf_listener)
         except RuntimeError as e:
             self._action_server.set_aborted(text=e.message)
             return
 
+        rospy.loginfo('fitting planes')
+        plane_list, filtered_cloud = self._plane_segmenter.find_planes(transformed_cloud_msg)
+        if self._filtered_cloud_pub.get_num_connections() > 0:
+            self._filtered_cloud_pub.publish(filtered_cloud)
+
         rospy.loginfo('creating action result and setting success')
-        result = ImageDetectionActionServer._get_action_result(transformed_cloud_msg, bounding_boxes,
+        result = ImageDetectionActionServer._get_action_result(transformed_cloud_msg, plane_list, bounding_boxes,
                                                                classes, confidences)
         self._action_server.set_succeeded(result)
 
     def _cloud_callback(self, cloud_msg):
         self._cloud_msg = cloud_msg
 
-    def _transform_cloud(self, cloud_msg):
-        """ TODO(minhnh) change this with transform_cloud_with_listener()
-        """
-        try:
-            common_time = self._tf_listener.getLatestCommonTime(self._target_frame, cloud_msg.header.frame_id)
-            cloud_msg.header.stamp = common_time
-            self._tf_listener.waitForTransform(self._target_frame, cloud_msg.header.frame_id,
-                                               cloud_msg.header.stamp, rospy.Duration(1))
-            tf_matrix = self._tf_listener.asMatrix(self._target_frame, cloud_msg.header)
-        except(tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            raise RuntimeError('Unable to transform {0} -> {1}'.format(cloud_msg.header.frame_id, self._target_frame))
-
-        return transform_point_cloud(cloud_msg, tf_matrix, self._target_frame)
+    def _plane_fitting_config_cb(self, config, _):
+        self._plane_segmenter.set_params(config)
+        return config
 
     @staticmethod
-    def _get_action_result(cloud_msg, bounding_boxes, classes, confidences):
+    def _get_action_result(cloud_msg, plane_list, bounding_boxes, classes, confidences):
         """
+        TODO(minhnh) adapt to multiple planes
         :type cloud_msg: PointCloud2
+        :type plane_list: PlaneList
         :type bounding_boxes: list
         :type classes: list
         :type confidences: list
         :rtype: DetectSceneResult
         """
         result = DetectSceneResult()
-        plane = Plane()
+        plane = plane_list.planes[0]
         for index, box in enumerate(bounding_boxes):
             detected_obj = Object()
             detected_obj.name = classes[index]
