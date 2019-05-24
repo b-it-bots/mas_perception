ace659bfc0fe0c272fa37f187f68e048ef4011b5
Mohammad Wasil <mwasil@outlook.co.id>
2019-05-24 17:44:17 +0200

    Add coefficient reference

--- mcr_scene_segmentation/common/include/mcr_scene_segmentation/scene_segmentation.h
+++ mcr_scene_segmentation/common/include/mcr_scene_segmentation/scene_segmentation.h
@@ -34,8 +34,10 @@ public:
     virtual ~SceneSegmentation();
 
     PointCloud::Ptr segment_scene(const PointCloud::ConstPtr &cloud, std::vector<PointCloud::Ptr> &clusters,
-    std::vector<mas_perception_libs::BoundingBox> &boxes, double &workspace_height);
-    PointCloud::Ptr findPlane(const PointCloud::ConstPtr &cloud, PointCloud::Ptr &hull, double &workspace_height);
+                                  std::vector<mas_perception_libs::BoundingBox> &boxes, 
+                                  Eigen::Vector4f &model_coefficients, double &workspace_height);
+    PointCloud::Ptr findPlane(const PointCloud::ConstPtr &cloud, PointCloud::Ptr &hull,
+                              Eigen::Vector4f &coefficients, double &workspace_height);
 
     void setCloudFilterParams(const mpl::CloudFilterParams&);
     void setPlaneSegmenterParams(const mpl::SacPlaneSegmenterParams&);
@@ -44,9 +46,7 @@ public:
     void setClusterParams(double cluster_tolerance, int cluster_min_size, int cluster_max_size,
                           double cluster_min_height, double cluster_max_height,  double max_length,
                           double cluster_min_distance_to_polygon);
-public:
-    // Make model coefficients public for 3d bbox from rgb bbox propossal
-    Eigen::Vector4f coefficients_;
+
 };
 
 #endif  // MCR_SCENE_SEGMENTATION_SCENE_SEGMENTATION_H
